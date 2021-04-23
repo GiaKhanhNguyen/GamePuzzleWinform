e1925d0e5546fd0f9d6b1c54dc2ac4426b591dd9
GiaKhanh <hoanggiakhanh147@gmail.com>
2021-04-23 10:07:26 +0700

    no message

---
+++ GameXepHinh/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace GameXepHinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBmain = new System.Windows.Forms.PictureBox();
+            this.pB9 = new System.Windows.Forms.PictureBox();
+            this.pB8 = new System.Windows.Forms.PictureBox();
+            this.pB7 = new System.Windows.Forms.PictureBox();
+            this.pB6 = new System.Windows.Forms.PictureBox();
+            this.pB5 = new System.Windows.Forms.PictureBox();
+            this.pB4 = new System.Windows.Forms.PictureBox();
+            this.pB3 = new System.Windows.Forms.PictureBox();
+            this.pB2 = new System.Windows.Forms.PictureBox();
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            this.btn_new = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_tamdung = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBmain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBmain
+            // 
+            this.pBmain.Location = new System.Drawing.Point(444, 25);
+            this.pBmain.Name = "pBmain";
+            this.pBmain.Size = new System.Drawing.Size(200, 200);
+            this.pBmain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBmain.TabIndex = 9;
+            this.pBmain.TabStop = false;
+            // 
+            // pB9
+            // 
+            this.pB9.Location = new System.Drawing.Point(231, 230);
+            this.pB9.Name = "pB9";
+            this.pB9.Size = new System.Drawing.Size(100, 100);
+            this.pB9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB9.TabIndex = 8;
+            this.pB9.TabStop = false;
+            // 
+            // pB8
+            // 
+            this.pB8.Location = new System.Drawing.Point(128, 230);
+            this.pB8.Name = "pB8";
+            this.pB8.Size = new System.Drawing.Size(100, 100);
+            this.pB8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB8.TabIndex = 7;
+            this.pB8.TabStop = false;
+            // 
+            // pB7
+            // 
+            this.pB7.Location = new System.Drawing.Point(25, 230);
+            this.pB7.Name = "pB7";
+            this.pB7.Size = new System.Drawing.Size(100, 100);
+            this.pB7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB7.TabIndex = 6;
+            this.pB7.TabStop = false;
+            // 
+            // pB6
+            // 
+            this.pB6.Location = new System.Drawing.Point(231, 127);
+            this.pB6.Name = "pB6";
+            this.pB6.Size = new System.Drawing.Size(100, 100);
+            this.pB6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB6.TabIndex = 5;
+            this.pB6.TabStop = false;
+            // 
+            // pB5
+            // 
+            this.pB5.Location = new System.Drawing.Point(128, 127);
+            this.pB5.Name = "pB5";
+            this.pB5.Size = new System.Drawing.Size(100, 100);
+            this.pB5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB5.TabIndex = 4;
+            this.pB5.TabStop = false;
+            // 
+            // pB4
+            // 
+            this.pB4.Location = new System.Drawing.Point(25, 127);
+            this.pB4.Name = "pB4";
+            this.pB4.Size = new System.Drawing.Size(100, 100);
+            this.pB4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB4.TabIndex = 3;
+            this.pB4.TabStop = false;
+            // 
+            // pB3
+            // 
+            this.pB3.Location = new System.Drawing.Point(231, 25);
+            this.pB3.Name = "pB3";
+            this.pB3.Size = new System.Drawing.Size(100, 100);
+            this.pB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB3.TabIndex = 2;
+            this.pB3.TabStop = false;
+            // 
+            // pB2
+            // 
+            this.pB2.Location = new System.Drawing.Point(128, 25);
+            this.pB2.Name = "pB2";
+            this.pB2.Size = new System.Drawing.Size(100, 100);
+            this.pB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB2.TabIndex = 1;
+            this.pB2.TabStop = false;
+            // 
+            // pB1
+            // 
+            this.pB1.Location = new System.Drawing.Point(25, 25);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(100, 100);
+            this.pB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB1.TabIndex = 0;
+            this.pB1.TabStop = false;
+            // 
+            // btn_new
+            // 
+            this.btn_new.Location = new System.Drawing.Point(495, 277);
+            this.btn_new.Name = "btn_new";
+            this.btn_new.Size = new System.Drawing.Size(75, 24);
+            this.btn_new.TabIndex = 11;
+            this.btn_new.Text = "New Game";
+            this.btn_new.UseVisualStyleBackColor = true;
+            this.btn_new.Click += new System.EventHandler(this.btn_new_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(587, 277);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 24);
+            this.btn_exit.TabIndex = 11;
+            this.btn_exit.Text = "Thoát";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // btn_tamdung
+            // 
+            this.btn_tamdung.Location = new System.Drawing.Point(403, 278);
+            this.btn_tamdung.Name = "btn_tamdung";
+            this.btn_tamdung.Size = new System.Drawing.Size(75, 23);
+            this.btn_tamdung.TabIndex = 12;
+            this.btn_tamdung.Text = "Tạm Dừng";
+            this.btn_tamdung.UseVisualStyleBackColor = true;
+            this.btn_tamdung.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btn_XulyMove_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(684, 411);
+            this.Controls.Add(this.btn_tamdung);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_new);
+            this.Controls.Add(this.pBmain);
+            this.Controls.Add(this.pB9);
+            this.Controls.Add(this.pB8);
+            this.Controls.Add(this.pB7);
+            this.Controls.Add(this.pB6);
+            this.Controls.Add(this.pB5);
+            this.Controls.Add(this.pB4);
+            this.Controls.Add(this.pB3);
+            this.Controls.Add(this.pB2);
+            this.Controls.Add(this.pB1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xếp Hình";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBmain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB1;
+        private System.Windows.Forms.PictureBox pB2;
+        private System.Windows.Forms.PictureBox pB3;
+        private System.Windows.Forms.PictureBox pB4;
+        private System.Windows.Forms.PictureBox pB5;
+        private System.Windows.Forms.PictureBox pB6;
+        private System.Windows.Forms.PictureBox pB7;
+        private System.Windows.Forms.PictureBox pB8;
+        private System.Windows.Forms.PictureBox pB9;
+        private System.Windows.Forms.PictureBox pBmain;
+        private System.Windows.Forms.Button btn_new;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_tamdung;
+    }
+}
+
---
+++ GameXepHinh/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameXepHinh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Init();
+            Setmap();
+            ShowpB();
+        }
+        private int x, y;
+        private int[] A = new int[9];
+        //khởi tạo giá trị ban đầu cho mảng có giá trị từ 1-9 và các giá trị ban đầu của biến
+        private void InitA()
+        {
+            for(int i = 1; i <= 9; i++)
+            {
+                A[i - 1] = i;
+            }
+        }
+        private void Init()
+        {
+            x = y = 2;
+            InitA();
+            pBmain.Image = GameXepHinh.Properties.Resources.main;
+        }
+        //hàm trả về vị trí của picture trên mảng
+        private int getvt(int x, int y)
+        {
+            return x + 3 * y;
+        }
+        //hàm load ảnh ra đúng với giá trị
+        private void getanh(PictureBox pB, int a)
+        {
+            switch(a)
+            {
+                case 1: pB.Image = GameXepHinh.Properties.Resources._1; break;
+                case 2: pB.Image = GameXepHinh.Properties.Resources._2; break;
+                case 3: pB.Image = GameXepHinh.Properties.Resources._3; break;
+                case 4: pB.Image = GameXepHinh.Properties.Resources._4; break;
+                case 5: pB.Image = GameXepHinh.Properties.Resources._5; break;
+                case 6: pB.Image = GameXepHinh.Properties.Resources._6; break;
+                case 7: pB.Image = GameXepHinh.Properties.Resources._7; break;
+                case 8: pB.Image = GameXepHinh.Properties.Resources._8; break;
+                case 9: pB.Image = null; pB.BackColor = Color.White; break;
+            }
+        }
+        //hàm load ảnh lên picturebox
+        private void ShowpB()
+        {
+            getanh(pB1, A[0]);
+            getanh(pB2, A[1]);
+            getanh(pB3, A[2]);
+            getanh(pB4, A[3]);
+            getanh(pB5, A[4]);
+            getanh(pB6, A[5]);
+            getanh(pB7, A[6]);
+            getanh(pB8, A[7]);
+            getanh(pB9, A[8]);
+        }
+        //hàm di chuyển các hình ảnh
+        private void Move_left()
+        {
+            if(x > 0)
+            {
+                int t = A[getvt(x, y)];
+                A[getvt(x, y)] = A[getvt(x - 1, y)];
+                A[getvt(x - 1, y)] = t;
+                x--;
+                ShowpB();
+            }
+        }
+        private void Move_right()
+        {
+            if (x < 2)
+            {
+                int t = A[getvt(x, y)];
+                A[getvt(x, y)] = A[getvt(x + 1, y)];
+                A[getvt(x + 1, y)] = t;
+                x++;
+                ShowpB();
+            }
+        }
+        private void Move_top()
+        {
+            if (y > 0)
+            {
+                int t = A[getvt(x, y)];
+                A[getvt(x, y)] = A[getvt(x, y - 1)];
+                A[getvt(x, y - 1)] = t;
+                y--;
+                ShowpB();
+            }
+        }
+        private void Move_bottom()
+        {
+            if (y < 2)
+            {
+                int t = A[getvt(x, y)];
+                A[getvt(x, y)] = A[getvt(x, y + 1)];
+                A[getvt(x, y + 1)] = t;
+                y++;
+                ShowpB();
+            }
+        }
+        //hàm xáo trộn các hình ảnh
+        Random rd = new Random();
+        private void Setmap()
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                int rrd = rd.Next(1, 5);
+                switch(rrd)
+                {
+                    case 1: Move_top(); break;
+                    case 2: Move_right(); break;
+                    case 3: Move_left(); break;
+                    case 4: Move_bottom(); break;
+                }
+            }
+            int t = A[getvt(x, y)];
+            A[getvt(x, y)] = A[8];
+            A[8] = t;
+            x = 2;
+            y = 2;
+            ShowpB();
+        }
+        //hàm xử lý di chuyển
+        private void btn_XulyMove_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                Move_right();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                Move_left();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                Move_bottom();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                Move_top();
+            }
+            if(checkwin() == true)
+            {
+                MessageBox.Show("Bạn Thật Giỏi");
+                Init();
+                Setmap();
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_new_Click(object sender, EventArgs e)
+        {
+            Init();
+            Setmap();
+        }
+        private bool checkwin()
+        {
+            for(int i = 1; i < 10; i++)
+            {
+                if (A[i - 1] != i)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
